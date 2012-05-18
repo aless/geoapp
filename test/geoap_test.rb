ac249d1b9f36d0fa3b0f71fe7253b3688a01c8b4
Alex Lorca <alex.lorca@gmail.com>
2012-05-18 18:33:34 -0400

    update geocoding test

--- test/geoap_test.rb
+++ test/geoap_test.rb
@@ -15,15 +15,13 @@ class MyAppTest < Test::Unit::TestCase
     assert last_response.body.include? "map_canvas"
   end
 
-  def test_js
-    get '/geoapp.js'
-    assert last_response.ok?
-    assert last_response.headers["Content-Type"].include? "text/javascript"
-  end
-
   def test_geocoding
     addr = "Hernando de aguirre 201, Santiago, Chile"
-    coord = {"lat"=>-33.4198636, "lng"=>-70.6009387}
+    coord = {
+      "lat" => -33.4198636,
+      "lng" => -70.6009387,
+      "txt" => "Hernando de Aguirre 201, Providencia, Santiago Metropolitan Region, Chile"
+    }
 
     get "/geocode?d=" + CGI::escape(addr)
     assert JSON.parse(last_response.body) == coord
